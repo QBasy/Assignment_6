cf3f564d726c6386d96d1e00196f2e42fa029a45
QoidynBasy <c0mplexie1@gmail.com>
2023-05-31 23:24:58 +0600

    commit)

--- Test.java
+++ Test.java
@@ -2,58 +2,63 @@ import java.util.Scanner;
 
 public class Test
 {
-    public static void main(String[] args) {
-        WeightedGraph<Integer> graph = new WeightedGraph<>();
+    public static void main(String[] args)
+    {
+        WeightedGraph<Vertex<Integer>> graph = new WeightedGraph<>();
 
         Scanner scanner = new Scanner(System.in);
         boolean exit = false;
 
-        while (!exit) {
+        while (!exit)
+        {
             System.out.println("Enter your choice:");
             System.out.println("1. Add vertex\n2. Add edge\n3. Print graph\n4. Breadth-First Search\n5. Dijkstra's Algorithm");
             System.out.println("6. Exit");
 
             int choice = scanner.nextInt();
-            switch (choice) {
-                case 1:
+            switch (choice)
+            {
+                case 1 -> {
                     System.out.println("Enter vertex value:");
-                    int vertex = scanner.nextInt();
+                    int vertexValue = scanner.nextInt();
+                    Vertex<Integer> vertex = new Vertex<>(vertexValue);
                     graph.addVertex(vertex);
-                    System.out.println("Vertex added");
-                    break;
-                case 2:
-                    System.out.println("Enter source vertex:");
-                    int source = scanner.nextInt();
-                    System.out.println("Enter destination vertex:");
-                    int destination = scanner.nextInt();
+                    System.out.println("Vertex added.");
+                }
+                case 2 -> {
+                    System.out.println("Enter source vertex value:");
+                    int sourceValue = scanner.nextInt();
+                    System.out.println("Enter destination vertex value:");
+                    int desValue = scanner.nextInt();
                     System.out.println("Enter edge weight:");
                     double weight = scanner.nextDouble();
+                    Vertex<Integer> source = new Vertex<>(sourceValue);
+                    Vertex<Integer> destination = new Vertex<>(desValue);
                     graph.addEdge(source, destination, weight);
-                    System.out.println("Edge added");
-                    break;
-                case 3:
+                    System.out.println("Edge added.");
+                }
+                case 3 -> {
                     System.out.println("Graph:");
                     graph.printGraph();
-                    break;
-                case 4:
-                    System.out.println("Enter start vertex for BreadthFirstSearch:");
-                    int startBFS = scanner.nextInt();
-                    BreadthFirstSearch<Integer> bfs = new BreadthFirstSearch<>();
+                }
+                case 4 -> {
+                    System.out.println("Enter starting vertex value for BreadthFirstSearch:");
+                    int startBFSValue = scanner.nextInt();
+                    Vertex<Integer> startBFS = new Vertex<>(startBFSValue);
+                    BreadthFirstSearch<Vertex<Integer>> bfs = new BreadthFirstSearch<>();
                     System.out.println("Breadth-First Search traversal:");
                     System.out.println(bfs.traverse(graph, startBFS));
-                    break;
-                case 5:
-                    System.out.println("Enter start vertex for DijkstraSearch:");
-                    int startDijkstra = scanner.nextInt();
-                    DijkstraSearch<Integer> dijkstra = new DijkstraSearch<>();
-                    System.out.println("DijkstraSearch traversal:");
+                }
+                case 5 -> {
+                    System.out.println("Enter starting vertex value for DijkstraSearch:");
+                    int startDijkstraValue = scanner.nextInt();
+                    Vertex<Integer> startDijkstra = new Vertex<>(startDijkstraValue);
+                    DijkstraSearch<Vertex<Integer>> dijkstra = new DijkstraSearch<>();
+                    System.out.println("DijkstraSearch Algorithm traversal:");
                     System.out.println(dijkstra.traverse(graph, startDijkstra));
-                    break;
-                case 6:
-                    exit = true;
-                    break;
-                default:
-                    System.out.println("Don't be a 🤡");
+                }
+                case 6 -> exit = true;
+                default -> System.out.println("Why can't you not be a 🤡");
             }
             System.out.println();
         }
